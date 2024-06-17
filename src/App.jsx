6eca3b4930469612d1294f4a5925111569d6333c
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-06-17 18:47:52 +0530

    ApplAyout

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Dashboard from "./Pages/Dashboard";
 import PageNotFound from "./Pages/PageNotFound";
 import Login from "./Pages/Login";
+import AppLayout from "./Pages/AppLayout";
 
 function App() {
 
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="dashboard" element={<Dashboard />} />
+        <Route
+          element={
+            // <ProtectedRoute>
+            <AppLayout />
+            // </ProtectedRoute>
+          }
+        >
+
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
-        <Dashboard />
+
       </Routes>
     </BrowserRouter>
   )
