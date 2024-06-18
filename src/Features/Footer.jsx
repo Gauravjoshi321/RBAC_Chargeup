70c786a6339a61b27ad259483b076a170b5ca90b
GAURAV JOSHI <gauravjoshi7060331206@gmail.com>
2024-06-18 19:39:15 +0530

    done with the frontend

--- src/Features/Footer.jsx
+++ src/Features/Footer.jsx
@@ -1,160 +1,110 @@
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
-  Transition,
-} from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import styled from "styled-components"
 
-const navigation = [
-  { name: 'Dashboard', href: '#', current: true },
-  { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-]
+const FooterContainer = styled.div`
+  min-height: 600px;
+  background-color: #0a1e2f;
+`;
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
-}
+const FooterBackground = styled.div`
+  background-image: linear-gradient(
+      to right bottom,
+      rgba(5, 18, 35, 0.874),
+      rgba(6, 24, 44, 0.9)
+    ),
+    url("EV-footer.jpeg");
+  height: 600px;
+  background-size: cover;
+  background-position: top;
+  display: flex;
+  align-items: center;
+`
+
+const FooterGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  place-items: center;
+  min-height: 500px;
+  width: 100%;
+  margin: 200px 150px;
+`;
+
+const Grid = styled.div`
+  color: #e0e0e0;
+  font-size: large;
+  font-weight: 500;
+
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const Image = styled.img`
+  height: 60px;
+  width: 60px;
+`;
+
+const Detail = styled.div`
+  font-size: 16px;
+`;
+
+const SocialMedia = styled.div`
+  display: flex;
+  gap: 10px;
+  margin-top: 2px;
+`;
+
+const Media = styled.a`
+`;
+
+const Img = styled.img`
+  height: 24px;
+  width: 24px;
+  background-color: #e8e8e8;
+  border-radius: 50%;
+  padding: 4px;
+`;
+
+const Heading = styled.h3`
+  font-size: larger;
+`
+
 
 export default function Footer() {
   return (
-    <Disclosure as="nav" className="bg-gray-800">
-      {({ open }) => (
-        <>
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <DisclosureButton className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </DisclosureButton>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                    alt="Your Company"
-                  />
-                </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    {navigation.map((item) => (
-                      <a
-                        key={item.name}
-                        href={item.href}
-                        className={classNames(
-                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'rounded-md px-3 py-2 text-sm font-medium',
-                        )}
-                        aria-current={item.current ? 'page' : undefined}
-                      >
-                        {item.name}
-                      </a>
-                    ))}
-                  </div>
-                </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                >
-                  <span className="absolute -inset-1.5" />
-                  <span className="sr-only">View notifications</span>
-                  <BellIcon className="h-6 w-6" aria-hidden="true" />
-                </button>
+    <FooterContainer>
+      <FooterBackground>
+        <FooterGrid>
+          <Grid>
+            <Image src="https://echargeup.com/wp-content/uploads/2023/01/favicon-1.webp" />
+            <Detail>18001230181</Detail>
+            <Detail>info@echargeup.com</Detail>
+            <Detail>300/3, 1st Floor, MG Road, Sultanpur, New Delhi-110030</Detail>
+            <SocialMedia>
+              <Media href="https://www.facebook.com/echargeup/">
+                <Img src="/FB.png" alt="facebook" />
+              </Media>
+              <Media href="https://www.linkedin.com/company/chargeup-solutions-pvt-ltd/">
+                <Img src="/LINKEDIN.png" alt="linkedin" />
+              </Media>
+              <Media href="https://www.instagram.com/e_chargeup/">
+                <Img src="INSTA.png" alt="instagram" />
+              </Media>
+              <Media href="https://www.youtube.com/channel/UCCSxPy0TYszJseg2j-a7m0g">
+                <Img src="YOUTUBE.png" alt="youtube" />
+              </Media>
+            </SocialMedia>
+          </Grid>
 
-                {/* Profile dropdown */}
-                <Menu as="div" className="relative ml-3">
-                  <div>
-                    <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </MenuButton>
-                  </div>
-                  <Transition
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      <MenuItem>
-                        {({ focus }) => (
-                          <a
-                            href="#"
-                            className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Your Profile
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ focus }) => (
-                          <a
-                            href="#"
-                            className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Settings
-                          </a>
-                        )}
-                      </MenuItem>
-                      <MenuItem>
-                        {({ focus }) => (
-                          <a
-                            href="#"
-                            className={classNames(focus ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                          >
-                            Sign out
-                          </a>
-                        )}
-                      </MenuItem>
-                    </MenuItems>
-                  </Transition>
-                </Menu>
-              </div>
+          <Grid>
+            <Heading>Useful Links</Heading>
+            <div style={{ display: "flex", flexDirection: "column", gap: "4px", fontSize: "16px" }}>
+              <Detail href="#">- Home</Detail>
+              <Detail href="#">- About</Detail>
+              <Detail href="#">- Contact</Detail>
             </div>
-          </div>
-
-          <DisclosurePanel className="sm:hidden">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              {navigation.map((item) => (
-                <DisclosureButton
-                  key={item.name}
-                  as="a"
-                  href={item.href}
-                  className={classNames(
-                    item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                    'block rounded-md px-3 py-2 text-base font-medium',
-                  )}
-                  aria-current={item.current ? 'page' : undefined}
-                >
-                  {item.name}
-                </DisclosureButton>
-              ))}
-            </div>
-          </DisclosurePanel>
-        </>
-      )}
-    </Disclosure>
+          </Grid>
+        </FooterGrid>
+      </FooterBackground>
+    </FooterContainer>
   )
 }
